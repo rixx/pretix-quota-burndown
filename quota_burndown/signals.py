68e1a2f5f8dd553ea86e6dc805c18384965e301a
Tobias Kunze <rixx@cutebit.de>
2016-10-08 19:05:29 +0200

    Add logic in signals.py

---
+++ quota_burndown/signals.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+import json
+
+from django.db.models import Q
+from django.dispatch import receiver
+from django.template.loader import get_template
+
+from pretix.base.models import Order, OrderPosition
+from pretix.control.signals import quota_detail_html
+
+
+@receiver(quota_detail_html, dispatch_uid='quota_burndown')
+def quota_burndown(sender, quota, **kwargs):
+    cache = sender.get_cache()
+    date_list = cache.get('quotaburndown_data_{}'.format(quota.name)) or []
+
+    if not date_list:
+        qs = OrderPosition.objects\
+            .filter(quota._position_lookup)\
+            .order_by('order__datetime')
+        first_day = qs.first().order.datetime.date()
+        last_day = qs.last().order.datetime.date()
+        duration = last_day - first_day
+
+        baseline = quota.count_blocking_vouchers()
+
+        for date in (first_day + timedelta(n) for n in range(duration.days + 1)):
+            date_list.append({
+                'date': date.strftime('%Y-%m-%d'),
+                'quota_used': qs.filter(
+                    Q(Q(order__status=Order.STATUS_PAID) | Q(order__status=Order.STATUS_PENDING)) &
+                    Q(Q(voucher__isnull=True) | Q(voucher__block_quota=False)) &
+                    Q(order__datetime__lte=date),
+                ).distinct().count() + baseline,
+            })
+        cache.set('quotaburndown_data_{}'.format(quota.name), date_list)
+
+    template = get_template('quotaburndown/quota_detail.html')
+    return template.render(context={'data': json.dumps(date_list)})
